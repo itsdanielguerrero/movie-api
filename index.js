dfa8d4917b98c1f648368397c4ffd7585d010861
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-11-19 11:58:06 -0500

    initial setup for this movie-api

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 1338
+
+app.get('/', (req, res) => {
+    res.send("Welcome to my Movie App!!!")
+})
+
+app.all('*', (req, res) => {
+    res.send("Oops looks like you did not find what you were looking for...")
+})
+
+app.listen(PORT, () => {
+    console.log("Server is up and running!")
+})
